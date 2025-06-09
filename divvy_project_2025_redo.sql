815a60f18d28303fb75c66a9dbd1d3b1d51c9376
Dalea Kumar <daleakumar92@outlook.com>
2025-06-10 03:27:36 +0530

    Update divvy_project_2025_redo.sql

--- divvy_project_2025_redo.sql
+++ divvy_project_2025_redo.sql
@@ -1,20 +1,20 @@
+01_view_combined_data_table.sql
+  
 SELECT* 
 FROM divvy_trips_pro2025.combined_divvy_pro255;
 
+02_create_table_backup_combined_divvy_pro255.sql
+  
 CREATE TABLE backup_combined_divvy_pro255 AS
 SELECT* FROM divvy_trips_pro2025.combined_divvy_pro255;
 
-SELECT* 
-FROM divvy_trips_pro2025.backup_combined_divvy_pro255;
-
+03_clean_backup_combined_divvy_pro255.sql
+  
 ALTER TABLE divvy_trips_pro2025.backup_combined_divvy_pro255
 ADD COLUMN ride_date DATE;
 
 UPDATE divvy_trips_pro2025.backup_combined_divvy_pro255
 SET ride_date = DATE(started_at);
-
-SELECT ride_date
-FROM divvy_trips_pro2025.backup_combined_divvy_pro255;
 
 SELECT ride_date,
 STR_TO_DATE(ride_date, '%Y-%m-%d')
